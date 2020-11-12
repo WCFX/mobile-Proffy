6069dc83f386035b8d108041e81a6184efad797c
wcfx <wcfx.dev@gmail.com>
2020-11-12 13:01:09 -0300

    Estilizacao da landing page concluida

--- App.tsx
+++ App.tsx
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
+
+import { Nunito_400Regular,Nunito_600SemiBold,Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { Poppins_400Regular,Poppins_600SemiBold,Poppins_700Bold } from '@expo-google-fonts/poppins';
+import { Archivo_400Regular,Archivo_600SemiBold,Archivo_700Bold} from '@expo-google-fonts/archivo';
 
 import Home from './src/screen/Home';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Archivo_400Regular,
+    Archivo_600SemiBold,
+    Archivo_700Bold
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
+  
   return (
     <>
       <Home />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </>
   );
 };
