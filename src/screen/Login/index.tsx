09fcc1bcfe8d70bccfd62e857a072557c14745de
wcfx <wcfx.dev@gmail.com>
2020-11-12 22:54:21 -0300

    Tela de login parcialmente criada.

--- src/screen/Login/index.tsx
+++ src/screen/Login/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { View, Text, TextInput } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import styles from './styles';
 
 const Login: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>      
+      <Text style={styles.title}>Login</Text>
+      <TextInput 
+        style={styles.input}
+        placeholder="Coloque seu login ou email aqui" />
+
+      <Text style={styles.title}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Coloque sua senha aqui" 
+        secureTextEntry={true}
+      />
+      <RectButton onPress={() => {}}>
+        <Text style={styles.forgottenPassword}>Esqueceu sua senha?</Text>
+      </RectButton>
     </View>
   )
 }
--- src/screen/Login/styles.ts
+++ src/screen/Login/styles.ts
@@ -9,9 +9,28 @@ const styles = StyleSheet.create({
   },
   title: {
     color: '#F9F9F9',
+    alignItems: 'center',
     fontSize: 32,
     width: 150,
+    marginTop: 40,
     fontFamily: 'Poppins_600SemiBold',
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: "#FFF",
+    borderRadius: 8,
+    fontSize: 18,
+    textAlign: 'center',
+    height: 50,
+    paddingHorizontal: 30, 
+    backgroundColor: "#FFF",
+    fontFamily: 'Poppins_400Regular',
+  },
+  forgottenPassword: {
+    textDecorationLine: 'underline',
+    marginHorizontal: 20,
+    marginVertical: 20,
+    color: '#FFF',
   },
 });
 
